c16b89536ac12365e180b28154f1ba0b86554164
Wurek <unset>
2026-05-19 21:55:11 +0200

    updated about section

--- js/aboutSlidesConfig.js
+++ js/aboutSlidesConfig.js
@@ -2,9 +2,6 @@
 // Supports simple HTML formatting: <b>bold</b>, <i>italic</i>, <u>underline</u>, <br> (line break)
 // Example: "I am a <b>creator</b> who loves building web experiences."
 const aboutSlides = [
-    "Urodzony w Błędowej Tyczyńskiej, obecnie mieszka i tworzy w Piekarach. Absolwent Akademii Sztuk Pięknych im. Jana Matejki w Krakowie (dyplom z rzeźby w pracowni prof. Jerzego Bandury, 1985).",
-    "Związany z Politechniką Krakowską, gdzie od 1989 roku pracuje na Wydziale Architektury jako profesor w Katedrze Rysunku, Malarstwa i Rzeźby. Wcześniej uczył rzeźby w krakowskim Liceum Sztuk Plastycznych.",
-    "Zajmuje się rzeźbą, medalierstwem i malarstwem. Autor 14 wystaw indywidualnych i uczestnik 33 zbiorowych. Wśród realizacji m.in. ołtarzowa „Pasja” dla Muzeum Powstania Warszawskiego oraz rzeźby sakralne w kościele Chrystusa Króla w Łękach Górnych.",
-    "Regularnie bierze udział w międzynarodowych plenerach „Wiklina w Arboretum”. Laureat wielu nagród. Jego prace znajdują się w zbiorach prywatnych, a także w Muzeum Sztuki Medalierskiej we Wrocławiu i Muzeum Dom Rodzinny Jana Pawła II w Wadowicach.",
-    "Członek Polskiego Stowarzyszenia Sztuki Medalierskiej i międzynarodowej federacji FIDEM."
+    "<p>Urodzony w Błędowej Tyczyńskiej, obecnie mieszka i tworzy w Piekarach. Absolwent Akademii Sztuk Pięknych im. Jana Matejki w Krakowie (dyplom z rzeźby w pracowni prof. Jerzego Bandury, 1985).</p><p>Związany z Politechniką Krakowską, gdzie od 1989 roku pracuje na Wydziale Architektury jako profesor w Katedrze Rysunku, Malarstwa i Rzeźby. Wcześniej uczył rzeźby w krakowskim Liceum Sztuk Plastycznych.</p><p>Zajmuje się rzeźbą, medalierstwem i malarstwem. Autor 14 wystaw indywidualnych i uczestnik 33 zbiorowych. Wśród realizacji m.in. ołtarzowa „Pasja” dla Muzeum Powstania Warszawskiego oraz rzeźby sakralne w kościele Chrystusa Króla w Łękach Górnych.</p><p>Regularnie bierze udział w międzynarodowych plenerach „Wiklina w Arboretum”. Laureat wielu nagród. Jego prace znajdują się w zbiorach prywatnych, a także w Muzeum Sztuki Medalierskiej we Wrocławiu i Muzeum Dom Rodzinny Jana Pawła II w Wadowicach.</p><p>Członek Polskiego Stowarzyszenia Sztuki Medalierskiej i międzynarodowej federacji FIDEM.",
+    "Jego prace rzeźbiarskie i medalierskie podzielone są na różne kategorie, które opisane są na następnych stronach. Zapraszamy:",
 ];
